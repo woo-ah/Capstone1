fd934cf6bd2a599f31e2dadadabe9d85da91ec52
woo-ah <a01052919770@gmail.com>
2024-07-30 23:38:19 +0900

    AuditingFields 코드 추가

--- src/main/java/com/example/capstone1/domain/AuditingFields.java
+++ src/main/java/com/example/capstone1/domain/AuditingFields.java
@@ -1,5 +1,46 @@
 package com.example.capstone1.domain;
 
 
-public class AuditingFields {
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public abstract class AuditingFields {
+
+    @Id
+    private String id;
+
+    @Column(nullable = false)
+    @Setter
+    protected String deleted;
+
+    @Column(nullable = true)
+    @Setter
+    protected String process;
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    protected LocalDateTime createdAt;
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    @LastModifiedDate
+    @Column(nullable = false)
+    protected LocalDateTime modifiedAt;
+
+    @PrePersist
+    public void onPrePersist(){
+        this.id = UUID.randomUUID().toString().replace("-", "");
+        this.deleted = "N";
+    }
+
+
 }
